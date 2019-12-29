2ee02bbb701fa446eaa43ff6837b40e3ec326827
spaceofmiah <geek_200@outlook.com>
2019-12-29 12:02:34 +0100

    Refactor Task Class

--- src/Task.js
+++ src/Task.js
@@ -1,39 +1,27 @@
 export default class Task {
 	constructor() {
-		let tasks = [];
+		let time = '';
+		let date = '';
+		let tags = [];
+		let title = '';
+		let priority = '';
+		let is_completed = false;
+		console.log("created successfully");
+		
 	}
 
-	/**
-	 * helps to add a task to list of tasks
-	 * @param {object} task
-	 */
-	addTask (task) {
-		console.log("calling addTask method");
-	};
+	updateTitle( title ) {
+		console.log("setting title");
+	}
 
-	/**
-	 * helps to remove a task from list of tasks
-	 * @param {integer} taskId
-	 */
-	removeTask (taskId) {
-		console.log("calling removeTask method");
-	};
+	updateDate( date ) {
+		console.log("setting date");
+	}
 
-	/**
-	 * helps to update a task
-	 * @param {integer} taskId
-	 */
-	updateTask (taskId) {
-		console.log("calling updateTask method");
-	};
-
-	/**
-	 * helps to mark a task as completed or uncompleted.
-	 * when this function is called on a completed task, it makes
-	 * that task to be uncompleted
-	 * @param {integer} taskId
-	 */
-	markTask (taskId) {
-		console.log("calling markTask method");
-	};
+	updateTags ( tags ) {
+		console.log("setting tags");
+	} 
+	completeTask(){ 
+		console.log("completing task") 
+	}
 }
